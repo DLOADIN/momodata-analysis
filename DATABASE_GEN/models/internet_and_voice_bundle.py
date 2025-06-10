8a66cd25a9c84ca747469bd6883460b03ada2229
DLOADIN <m.david@alustudent.com>
2025-06-10 20:01:22 +0200

    commit message

--- DATABASE_GEN/models/internet_and_voice_bundle.py
+++ DATABASE_GEN/models/internet_and_voice_bundle.py
@@ -4,7 +4,7 @@ from models.payments_base import PaymentBase
 
 
 class Bundles(Base, PaymentBase):
-    """model for a table that stores payment to buying bundles"""
+    """model for a table that stores payment to the buying bundles"""
 
     __tablename__ = "bundles_and_packs"
 
